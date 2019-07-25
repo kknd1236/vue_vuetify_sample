3608992d4f3c8fe9fd24ca040daf7170aaa92406
kknd1236 <kknd1236@empal.com>
2019-07-25 22:56:33 +0900

    first commit

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
+// import Home from "./views/Home.vue";
 
 Vue.use(Router);
 
@@ -11,16 +11,13 @@ export default new Router({
     {
       path: "/",
       name: "home",
-      component: Home
+      component: () => import(/* webpackChunkName: "home" */ "./views/Home.vue")
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
+      path: "/login",
+      name: "login",
       component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+        import(/* webpackChunkName: "login" */ "./views/Login.vue")
     }
   ]
 });
---
+++ src/views/Login.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-container fill-height style="max-width:450px;">
+    <v-layout align-center wrap>
+      <v-flex xs12>
+        <v-card>
+          <v-toolbar flat>
+            <v-toolbar-title>로그인</v-toolbar-title>
+          </v-toolbar>
+          <div class="pa-3">
+            <v-text-field v-model="email" label="이메일"></v-text-field>
+            <v-text-field
+              :type="'password'"
+              v-model="password"
+              label="비밀번호"
+            ></v-text-field>
+          </div>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: null,
+      password: null
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
